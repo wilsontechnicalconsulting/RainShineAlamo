d642bd625796f7d80aa954a172f4c696e9a158c5
Eric Wilson <EricWilson@erics-mbp-2.snofire7.local>
2016-08-19 16:00:59 -0700

    Xcode wants to update syntax to swift 3. Should aready be swift3, non compilable

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+# platform :ios, '9.0'
+use_frameworks!
+
+target 'RainShine' do
+ pod 'Alamofire',
+ 	:git => 'https://github.com/Alamofire/Alamofire.git',
+ 	:branch => 'swift3'
+
+  # Pods for RainShine
+
+end
--- RainShine/WeatherVC.swift
+++ RainShine/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  RainShine
 //
 //  Created by Eric Wilson on 8/19/16.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
